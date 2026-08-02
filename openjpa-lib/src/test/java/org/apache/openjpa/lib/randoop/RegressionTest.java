e409ccbaa6cea3478cf43fa1a70f343a422cf8fd
matteoavallone <matteo.avallone2000@gmail.com>
2026-08-02 22:30:51 +0200

    randoop lib

---
+++ openjpa-lib/src/test/java/org/apache/openjpa/lib/randoop/RegressionTest.java
@@ -0,0 +1,11 @@
+package org.apache.openjpa.lib.randoop;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTest0.class })
+public class RegressionTest {
+}
+
+
